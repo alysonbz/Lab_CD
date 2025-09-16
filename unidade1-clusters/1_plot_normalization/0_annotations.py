600d2ca60e0c9910c85b5c97fb600c64871d63bc
Gabriela <gabriela.costa@alu.ufc.br>
2025-09-16 14:46:53 -0300

    oii

---
+++ unidade1-clusters/1_plot_normalization/0_annotations.py
@@ -0,0 +1,7 @@
+from matplotlib import pyplot as plt
+
+x_coordinates = [80, 93, 86, 98, 86, 9, 15, 3, 10, 20, 44, 56, 49, 62, 44]
+y_coordinates = [87, 96, 95, 92, 92, 57, 49, 47, 59, 55, 25, 2, 10, 24, 10]
+
+plt.scatter(x_coordinates, y_coordinates)
+plt.show()
