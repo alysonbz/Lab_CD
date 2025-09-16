18d8c1f34d725c6ee888d6985613504772e7acc9
Hellen <hellenlavyne26@gmail.com>
2025-09-16 14:40:09 -0300

    feito

---
+++ unidade1-clusters/1_plot_normalization/0_annotations.py
@@ -0,0 +1,5 @@
+from matplotlib import pyplot as plt
+x_cordenadas = [80,93,86,98,86,9,15,3,10,20,44,56,49,62,44]
+y_cordenadas = [87,96,95,92,92,57,49,47,59,55,25,2,10,24,10]
+plt.scatter(x_cordenadas,y_cordenadas)
+plt.show()
