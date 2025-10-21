50c35af861f5109ff30d3bce263b14ebbab35624
jeftedamaceno <jeftedamaceno@gmail.com>
2025-10-21 15:11:16 -0300

    enviando atividade do dia 21/10

--- unidade2-NLP/1_regular_expressions/Atividade_regular_expression.py
+++ unidade2-NLP/1_regular_expressions/Atividade_regular_expression.py
@@ -14,4 +14,33 @@ text = "A liguagem de programação em Python é a mais utilizada do mundo," \
 
 print("resultado primeira questão: ",cont_word(text))
 
+#2. **email:**
+def valida_email(email):
+    padrao = r'[a-z]@[a-z].[a-z]'
+    resp = re.findall(padrao, email)
+    if resp == None or resp == []:
+        return f'{email} e invalido'
+    else:
+        return f'{email} e valido'
 
+email = 'jef-te@'
+print("resultado segunda questão: ",valida_email(email))
+
+#3. **telefone:**
+# def valida_telefone(telefone):
+#     padrao = r'([\d+])[\d+]-[\d+]'
+#     resp = re.findall(padrao, telefone)
+#     if resp == None or resp == []:
+#         return f'{telefone} e invalido'
+#     else:
+#         return f'{telefone} e valido'
+
+# telefone = '(88) 98101-4442'
+# print("resultado segunda questão: ",valida_telefone(telefone))
+# def extrai_telefone(telefone):
+#     padrao = r'('
+#     resp = re.split(padrao, telefone)
+#     return resp
+
+# telefone = '(88) 98101-4442'
+# print("resultado terceira questão: ",extrai_telefone(telefone))
