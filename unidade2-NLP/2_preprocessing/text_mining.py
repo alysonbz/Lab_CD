9075b0102019e5b88a666a357d5b477fda5c8647
Kauê Barbosa <kauebarbosa1k@gmail.com>
2025-12-02 07:54:40 -0300

    commit

--- unidade2-NLP/2_preprocessing/text_mining.py
+++ unidade2-NLP/2_preprocessing/text_mining.py
@@ -1,2 +1,4 @@
 from src.utils import get_sample_article
+from nltk.tokenize import sent_tokenize, word_tokenize
+from collections import Counter
 
--- unidade2-NLP/3_text_classification/1_countVectorizer.py
+++ unidade2-NLP/3_text_classification/1_countVectorizer.py
@@ -1,8 +1,8 @@
 
 from src.utils import load_fake_news_dataset
 # Import the necessary modules
-____
-____
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
 
 df = load_fake_news_dataset()
 
@@ -16,10 +16,10 @@ y = df['label']
 X_train, X_test, y_train, y_test = train_test_split(df["text"],y,test_size = 0.3,random_state = 53)
 
 # Initialize a CountVectorizer object: count_vectorizer
-count_vectorizer = ____
+count_vectorizer = CountVectorizer()
 
 # # Transform the training data using X_train values  with fit_transform
-count_train = ____
+count_train =
 
 # Transform the test data using transform function
 count_test = ____
