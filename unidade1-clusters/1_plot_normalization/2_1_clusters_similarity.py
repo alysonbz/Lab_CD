b4ed46957d151aa243bbcd34198ccdfa8b65e9ef
KevBoyz <kevboyz@pm.me>
2026-08-31 15:48:36 -0300

    lapada de coisas

--- unidade1-clusters/1_plot_normalization/2_1_clusters_similarity.py
+++ unidade1-clusters/1_plot_normalization/2_1_clusters_similarity.py
@@ -2,30 +2,65 @@ from src.utils import load_pokemon_dataset
 import numpy as np
 
 
-def compute_single_linkage(cluster1,cluster2):
-     return None
+def compute_single_linkage(cluster1, cluster2):
+    min_dist = float('inf')
+    for p1 in cluster1:
+        for p2 in cluster2:
+            dist = np.linalg.norm(np.array(p1) - np.array(p2))
+            if dist < min_dist:
+                min_dist = dist
+    return min_dist
+
 
 def compute_complete_linkage(cluster1, cluster2):
-     return None
+    max_dist = 0
+    for p1 in cluster1:
+        for p2 in cluster2:
+            dist = np.linalg.norm(np.array(p1) - np.array(p2))
+            if dist > max_dist:
+                max_dist = dist
+    return max_dist
+
 
 def compute_average_linkage(cluster1, cluster2):
-     return None
-
-def compute_centroid_linkage(cluster1,cluster2):
-    return None
-
-def compute_ward_linkage(cluster1,cluster2):
-    return None
-
-
-cluster1 = [[9.0,8.0],[6.0,4.0],[2.0,10.0],[3.0,6.0],[1.0,0.0]]
-cluster2 = [[7.0,4.0],[1.0,10.0],[6.0,10.0],[1.0,6.0],[7.0,1.0]]
-
-print("similaridade ligação simples: ", compute_single_linkage(cluster1,cluster2))
-print("similaridade ligação completa: ", compute_complete_linkage(cluster1,cluster2))
-print("similaridade ligação média: ", compute_average_linkage(cluster1,cluster2))
-print("similaridade pelo método do centroide: ", compute_centroid_linkage(cluster1,cluster2))
-print("similaridade ligação simples: ", compute_ward_linkage(cluster1,cluster2))
+    total_dist = 0
+    count = 0
+    for p1 in cluster1:
+        for p2 in cluster2:
+            dist = np.linalg.norm(np.array(p1) - np.array(p2))
+            total_dist += dist
+            count += 1
+    return total_dist / count
 
 
+def compute_centroid_linkage(cluster1, cluster2):
+    centroid1 = np.mean(cluster1, axis=0)
+    centroid2 = np.mean(cluster2, axis=0)
+    return np.linalg.norm(centroid1 - centroid2)
 
+
+def compute_ward_linkage(cluster1, cluster2):
+    centroid1 = np.mean(cluster1, axis=0)
+    centroid2 = np.mean(cluster2, axis=0)
+    centroid_merged = np.mean(np.array(cluster1 + cluster2), axis=0)
+
+    n1 = len(cluster1)
+    n2 = len(cluster2)
+
+    dist = (n1 * n2) / (n1 + n2) * np.linalg.norm(centroid1 - centroid2) ** 2
+    return np.sqrt(dist)
+
+
+cluster1 = [[9.0, 8.0], [6.0, 4.0], [2.0, 10.0], [3.0, 6.0], [1.0, 0.0]]
+cluster2 = [[7.0, 4.0], [1.0, 10.0], [6.0, 10.0], [1.0, 6.0], [7.0, 1.0]]
+
+print("similaridade ligação simples: ",
+      compute_single_linkage(cluster1, cluster2))
+print("similaridade ligação completa: ",
+      compute_complete_linkage(cluster1, cluster2))
+print("similaridade ligação média: ",
+      compute_average_linkage(cluster1, cluster2))
+print("similaridade pelo método do centroide: ",
+      compute_centroid_linkage(cluster1, cluster2))
+print("similaridade ligação simples: ",
+      compute_ward_linkage(cluster1, cluster2))
--- unidade1-clusters/1_plot_normalization/3_1_manual_kmeans.py
+++ unidade1-clusters/1_plot_normalization/3_1_manual_kmeans.py
@@ -1,37 +1,68 @@
-from src.utils import load_pokemon_dataset
+import numpy as np
+import random
+from src.utils import loadpokemon_dataset_df
 
 
-df = load_pokemon_dataset()
+df = loadpokemon_dataset_df()
 
 
 def set_random_cluster_coordinate(num_of_cluster):
     coord_list = []
-    label_list = range(0,num_of_cluster)
-    #preecher a lista com quatro coordenadas aleatótias.
-    return coord_list , label_list
+    label_list = list(range(0, num_of_cluster))
+    for i in range(num_of_cluster):
+        coord_list.append([random.uniform(0, 100), random.uniform(0, 100)])
+    return coord_list, label_list
+
 
 def create_points(df):
-    coords = [] # [ [x1,y1], [x2,y2] , [x3,y3]....]
+    coords = []
+    for index, row in df.iterrows():
+        coords.append([row['sliding_tackle'], row['aggression']])
     return coords
 
-def dist_euclidian(p1,p2):
-    dist = 0
+
+def dist_euclidian(p1, p2):
+    dist = np.sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)
     return dist
 
-def kmeans(df,num_of_cluster):
 
-    centroids ,  centroids_labels = set_random_cluster_coordinate(num_of_cluster)
+def kmeans(df, num_of_cluster):
+    centroids,  centroids_labels = set_random_cluster_coordinate(
+        num_of_cluster)
     increase_cluster = True
-    coords  = create_points(df)
-    coord_label = []  #
-    while increase_cluster == True:
+    coords = create_points(df)
+    coord_label = []
+    iterations = 0
+    max_iterations = 100
+
+    while increase_cluster == True and iterations < max_iterations:
+        iterations += 1
+        old_centroids = [c[:] for c in centroids]
+        coord_label = []
+
+        # Atribuir cada ponto ao centroide mais proximo
         for coord in coords:
-           coord_label.append(None)
-            ##algoritmo
+            min_dist = float('inf')
+            closest_centroid = 0
+            for i, centroid in enumerate(centroids):
+                d = dist_euclidian(coord, centroid)
+                if d < min_dist:
+                    min_dist = d
+                    closest_centroid = i
+            coord_label.append(closest_centroid)
 
-    return  coord_label
+        # Recalcular centroides
+        for i in range(num_of_cluster):
+            cluster_points = [coords[j]
+                              for j in range(len(coords)) if coord_label[j] == i]
+            if cluster_points:
+                centroids[i] = np.mean(cluster_points, axis=0).tolist()
 
+        # Verificar convergencia
+        if np.allclose(old_centroids, centroids):
+            increase_cluster = False
 
+    return coord_label
 
 
 num_of_clusters = 2
