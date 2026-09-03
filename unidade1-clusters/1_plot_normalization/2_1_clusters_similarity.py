dcdf1b2768bf3839f1f56f0ee7b6ec8ff4afa9e7
Maria <mariadfatima927@gmail.com>
2026-09-03 14:53:57 -0300

    Questão 2_1

--- unidade1-clusters/1_plot_normalization/2_1_clusters_similarity.py
+++ unidade1-clusters/1_plot_normalization/2_1_clusters_similarity.py
@@ -3,19 +3,51 @@ import numpy as np
 
 
 def compute_single_linkage(cluster1,cluster2):
-     return None
+    distancias = []
+
+    for ponto1 in cluster1:
+        for ponto2 in cluster2:
+            distancia = np.linalg.norm(np.array(ponto1) - np.array(ponto2))
+            distancias.append(distancia)
+
+    return min(distancias)
 
 def compute_complete_linkage(cluster1, cluster2):
-     return None
+    distancias = []
+
+    for ponto1 in cluster1:
+        for ponto2 in cluster2:
+            distancia = np.linalg.norm(np.array(ponto1) - np.array(ponto2))
+            distancias.append(distancia)
+
+    return max(distancias)
 
 def compute_average_linkage(cluster1, cluster2):
-     return None
+    distancias = []
 
-def compute_centroid_linkage(cluster1,cluster2):
-    return None
+    for ponto1 in cluster1:
+        for ponto2 in cluster2:
+            distancia = np.linalg.norm(np.array(ponto1) - np.array(ponto2))
+            distancias.append(distancia)
 
-def compute_ward_linkage(cluster1,cluster2):
-    return None
+    return np.mean(distancias)
+
+def compute_centroid_linkage(cluster1, cluster2):
+    centroide1 = np.mean(cluster1, axis=0)
+    centroide2 = np.mean(cluster2, axis=0)
+
+    return np.linalg.norm(centroide1 - centroide2)
+
+def compute_ward_linkage(cluster1, cluster2):
+    quantidade1 = len(cluster1)
+    quantidade2 = len(cluster2)
+
+    centroide1 = np.mean(cluster1, axis=0)
+    centroide2 = np.mean(cluster2, axis=0)
+
+    distancia = np.linalg.norm(centroide1 - centroide2)
+
+    return np.sqrt(2 * quantidade1 * quantidade2 / (quantidade1 + quantidade2)) * distancia
 
 
 cluster1 = [[9.0,8.0],[6.0,4.0],[2.0,10.0],[3.0,6.0],[1.0,0.0]]
@@ -26,6 +58,3 @@ print("similaridade ligação completa: ", compute_complete_linkage(cluster1,clu
 print("similaridade ligação média: ", compute_average_linkage(cluster1,cluster2))
 print("similaridade pelo método do centroide: ", compute_centroid_linkage(cluster1,cluster2))
 print("similaridade ligação simples: ", compute_ward_linkage(cluster1,cluster2))
-
-
-
