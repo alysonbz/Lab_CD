1980d6b91d47adc35ecb0772517ce7ba4586c29e
DarwinDontCare <jefsupergamer@gmail.com>
2026-09-04 15:20:02 -0300

    feat: activity 7_scaling_data.py

--- unidade1-clusters/1_plot_normalization/7_scaling_data.py
+++ unidade1-clusters/1_plot_normalization/7_scaling_data.py
@@ -1,14 +1,20 @@
+import sys
+from pathlib import Path
+
+root_path = Path(__file__).resolve().parents[2]
+if str(root_path) not in sys.path:
+    sys.path.append(str(root_path))
 
 from src.utils import load_wine_dataset
-from ___ import ___
+from sklearn.preprocessing import StandardScaler
 wine = load_wine_dataset()
 X = wine.drop(['Quality'],axis=1)
 
 # Create the scaler
-scaler = ____
+scaler = StandardScaler()
 
-X_norm = _____
+X_norm = scaler.fit_transform(X)
 
-print('variancia',__)
+print('variancia', X.var())
 
-print('variancia do dataset normalizado',__)
+print('variancia do dataset normalizado', X_norm.var())
