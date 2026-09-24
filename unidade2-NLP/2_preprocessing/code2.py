2316016cd46110d515a49ee519164c207386d2a7
aluno <ricardo112.41@gmail.com>
2026-09-24 15:03:32 -0300

    atividade

--- unidade2-NLP/2_preprocessing/code2.py
+++ unidade2-NLP/2_preprocessing/code2.py
@@ -1,26 +1,34 @@
 # Import WordNetLemmatizer and Counter
-__
-__
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from collections import Counter
 from src.utils import get_wiki_article_lower_tokens, get_english_stop_words
-
+nltk.download('wordnet')
+nltk.download('omw-1.4')
 
 lower_tokens = get_wiki_article_lower_tokens()
 
 # Retain alphabetic words: alpha_only
-alpha_only = [_ for t in __ if __]
+alpha_only = [t for t in lower_tokens
+              if t.isalpha()]
 
 english_stop = get_english_stop_words()
 # Remove all stop words: no_stops
-no_stops = [__ for t in __ if __ not in __]
+no_stops = [t for t in lower_tokens
+            if t not in get_english_stop_words()]
+pontuacao = ['.', ',', '"', "'", '?', '!', ':', ';', '(', ')', '[', ']', '{', '}',"``","''"]
+no_punctuation = [t for t in no_stops
+                  if t not in pontuacao]
 
 # Instantiate the WordNetLemmatizer
-wordnet_lemmatizer = __()
+wordnet_lemmatizer = WordNetLemmatizer()
 
 # Lemmatize all tokens into a new list: lemmatized
-lemmatized = [__(__) for t in __]
+lemmatized = [wordnet_lemmatizer.lemmatize(t) for t in no_punctuation]
 
 # Create the bag-of-words: bow
-bow = __
+bow = Counter(lemmatized)
 
 # Print the 10 most common tokens
-___
+print(bow.most_common(10))
