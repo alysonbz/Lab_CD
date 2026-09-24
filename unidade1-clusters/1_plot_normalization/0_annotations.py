23c5a31ffd0bd8c732a9a402359b2e57730d8232
SuianyGomes <suianypinto@gmail.com>
2026-09-24 14:35:48 -0300

    Atividade

--- unidade1-clusters/1_plot_normalization/0_annotations.py
+++ unidade1-clusters/1_plot_normalization/0_annotations.py
@@ -1,4 +1,4 @@
-import re
+#import re
 match_digits_and_words = ('(\d+|w+)')
 print(re.findall(match_digits_and_words,'He has 11 cats. Do you like cats?'))
 
@@ -13,3 +13,21 @@ parttern3 = '(#\d\W+\?!)'
 parttern4 = '\s+'
 partter = '?'
 print(re.findall(parttern4, "SOLIDER #1: Found them? In Mercea? The coconut's tropical!"))
+
+
+
+from nltk.tokenize import word_tokenize
+from collections import Counter
+from nltk.corpus import stopwords
+
+import nltk
+nltk.download('stopwords')
+
+
+text = """The cat is in the box. The cat likes the box. The box is over the cat."""
+tokens = [w for  w in word_tokenize(text.lower())
+            if w.isalpha()]
+no_stops = [t for t in tokens
+            if t not in stopwords.words('english')]
+Counter().most_common(2)
+print(Counter(no_stops).most_common(2))
